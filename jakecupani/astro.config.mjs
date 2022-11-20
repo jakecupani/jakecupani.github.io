ec485d95314f577e23d437e5c17b503eb1ba820e
Jake Cupani <jakecup1@gmail.com>
2022-11-20 13:57:22 -0500

    edits

--- jakecupani/astro.config.mjs
+++ jakecupani/astro.config.mjs
@@ -1,22 +1,22 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
+  site: "https://jakecupani.github.io",
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: "nord",
     },
     rehypePlugins: [
       [
-        'rehype-external-links',
+        "rehype-external-links",
         {
-          target: '_blank',
+          target: "_blank",
         },
       ],
     ],
   },
-})
+});
