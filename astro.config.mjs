94dfe63336784221f5aa02ddf53ab4d3fabd07e6
Jake Cupani <jakecup1@gmail.com>
2025-04-27 15:40:39 -0400

    transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-import preprocess from "svelte-preprocess";
-import postcss from 'postcss';
-
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
 
 export default defineConfig({
-  integrations: [
-    svelte({
-      preprocess: preprocess({
-        postcss: true // Force-enable PostCSS
-      })
-    })
-  ]
+  integrations: [svelte(), postcss()],
 });
