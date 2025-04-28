bb656c25a45722c5cde8d9c7bb94ef31589002a4
Jake Cupani <jakecup1@gmail.com>
2025-04-28 13:54:07 -0400

    transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import postcss from '@astrojs/postcss';
 
 export default defineConfig({
-  integrations: [svelte(), postcss()],
+  integrations: [svelte()],
 });
