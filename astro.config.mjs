78a7e8e855f2086fe07ff0cc265ac88f13af5afe
Jake Cupani <jakecup1@gmail.com>
2025-04-28 14:02:19 -0400

    transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
+
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jakecupani.github.io",
-  integrations: [mdx(), svelte()],
+  integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: "nord",
