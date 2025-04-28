4373a025a5397b186ef5130015c33aeffd1fdeff
Jake Cupani <jakecup1@gmail.com>
2025-04-28 14:00:22 -0400

    transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,22 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  site: "https://jakecupani.github.io",
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: "nord",
+    },
+    rehypePlugins: [
+      [
+        "rehype-external-links",
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+  },
 });
